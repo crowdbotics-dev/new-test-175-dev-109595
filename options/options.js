25557f15e2a2a83d9bfbd842439ea460ec8bbd3b
Dmitrii Alferev <dmitry@alferev.com>
2023-12-19 18:48:45 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "new-test-175-dev-109595",
   url: "https://new-test-175-dev-109595.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
